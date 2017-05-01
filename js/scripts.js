831378e99dc5e93ba12574bb867e1b1c8a694f58
john dowd and roger tobias <roger@>
2017-05-01 16:12:39 -0700

    this commit removes our debugging code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
   $("#groceryItems").submit(function(event) {
-    debugger;
+
     var arrayItems = ["itemOne", "itemTwo", "itemThree", "itemFour", "itemFive"];
-    console.log(11);
+
 
     var newArrayItems = arrayItems.map(function(arrayItem) {
       return $("input#" + arrayItem).val().toUpperCase();
 
     });
     newArrayItems.sort();
-    console.log(10);
+
     newArrayItems.forEach(function(newArrayItem) {
       $("#unordered").append("<li>" + newArrayItem + "</li>");
     });
-    console.log(12);
 
 
     $("#unordered").show();
