5eaf3d6eec91b31e05a07070067583f7a43ec074
john dowd and roger tobias <roger@>
2017-05-01 16:06:24 -0700

    added js, array and sort

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
   $("#groceryItems").submit(function(event) {
-
+    debugger;
     var arrayItems = ["itemOne", "itemTwo", "itemThree", "itemFour", "itemFive"];
+    console.log(11);
 
-    arrayItems.forEach(function(arrayitem) {
-      var userInput = $("input#" + arrayitem).val();
-      $("#unordered").append("<li>" + userInput + "</li>");
+    var newArrayItems = arrayItems.map(function(arrayItem) {
+      return $("input#" + arrayItem).val().toUpperCase();
+
     });
+    newArrayItems.sort();
+    console.log(10);
+    newArrayItems.forEach(function(newArrayItem) {
+      $("#unordered").append("<li>" + newArrayItem + "</li>");
+    });
+    console.log(12);
+
 
     $("#unordered").show();
 
